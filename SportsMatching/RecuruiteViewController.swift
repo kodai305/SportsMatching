e3c769c50d3b4a7445964fbf460121e5b98a03d9
Kodai Takagi <mj5824@icloud.com>
2018-08-05 18:45:58 +0900

    募集フォームページに投稿ボタンを追加(しただけ)

--- SportsMatching/RecuruiteViewController.swift
+++ SportsMatching/RecuruiteViewController.swift
@@ -66,6 +66,14 @@ class RecuruiteViewController: FormViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func postButton(_ sender: Any) {
+        // XXX: 投稿ボタンを押した際の処理を実装する
+        
+        // XXX: 必須項目の入力チェック
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
