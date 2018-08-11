8f3c5962d24ae139e4cd3983ff9e6dcdc6988657
user <matsuei>
2018-08-11 11:24:25 +0900

    検索条件が入力されていない場合に警告が出る様に設定

--- SportsMatching/SearchViewController.swift
+++ SportsMatching/SearchViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import Eureka
 import ImageRow
 import FirebaseFirestore
+import SVProgressHUD
 
 class SearchViewController: FormViewController {
 
@@ -22,12 +23,36 @@ class SearchViewController: FormViewController {
             <<< NameRow("events") {
                 $0.title = "種目"
                 $0.placeholder = "プルダウン？"
-                $0.value = "" //初期値
+                //$0.value = "" //初期値
             }
-            <<< TextRow("prefecture"){
+            <<< PushRow<String>("prefecture") {
                 $0.title = "都道府県"
-                $0.placeholder = "プルダウンにしたい"
-                $0.value = "" 
+                $0.options = ["北海道", "青森県", "岩手県", "宮城県", "秋田県",
+                              "山形県", "福島県", "茨城県", "栃木県", "群馬県",
+                              "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県",
+                              "富山県", "石川県", "福井県", "山梨県", "長野県",
+                              "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県",
+                              "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県",
+                              "鳥取県", "島根県", "岡山県", "広島県", "山口県",
+                              "徳島県", "香川県", "愛媛県", "高知県", "福岡県",
+                              "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県",
+                              "鹿児島県", "沖縄県"]
+                $0.selectorTitle = "都道府県"
+                }.onPresent { from, to in
+                    to.dismissOnSelection = false
+                    to.dismissOnChange = false
+                    to.sectionKeyForValue = { option in
+                        switch option {
+                        case "北海道", "青森県", "岩手県", "宮城県", "秋田県","山形県", "福島県": return "1北海道・東北"
+                        case "茨城県", "栃木県", "群馬県","埼玉県", "千葉県", "東京都", "神奈川県": return "2関東"
+                        case "新潟県","富山県", "石川県", "福井県", "山梨県", "長野県","岐阜県", "静岡県", "愛知県": return "3中部"
+                        case "三重県", "滋賀県","京都府", "大阪府", "兵庫県", "奈良県", "和歌山県": return "4関西"
+                        case "鳥取県", "島根県", "岡山県", "広島県", "山口県": return "5中国"
+                        case "徳島県", "香川県", "愛媛県", "高知県": return "6四国"
+                        case "福岡県","佐賀県", "長崎県", "熊本県", "大分県", "宮崎県","鹿児島県", "沖縄県": return "7九州・沖縄"
+                        default: return ""
+                        }
+                    }
         }
 
         // Do any additional setup after loading the view.
@@ -48,6 +73,24 @@ class SearchViewController: FormViewController {
             nextView.events = values["events"] as! String
             nextView.prefecture = values["prefecture"] as! String
         }
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == "toResultViewController" {
+            // タグ設定済みの全てのRowの値を取得
+            let values = form.values()
+            if values["events"].unsafelyUnwrapped == nil{
+                SVProgressHUD.showError(withStatus: "種目名を選択して下さい")
+                return false
+            }else{
+                if values["prefecture"].unsafelyUnwrapped == nil{
+                    SVProgressHUD.showError(withStatus: "都道府県を選択して下さい")
+                    return false
+                }
+            }
+            return true
+        }
+        return false
     }
 
     /*
