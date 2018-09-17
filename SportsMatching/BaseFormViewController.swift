3579f75cf4f9a8416ca57678eb2237f20e1155dd
user <matsuei>
2018-09-17 16:58:36 +0900

    投稿をUserDefaultsに保存＆画像をFirebaseStorageに保存する処理を関数化 （FireStoreへの保存もやる）

--- SportsMatching/BaseFormViewController.swift
+++ SportsMatching/BaseFormViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseStorage
 import Eureka
 
 class BaseFormViewController: FormViewController {
@@ -54,6 +55,57 @@ class BaseFormViewController: FormViewController {
             let defaults = UserDefaults.standard
             defaults.set(uid ,forKey: "UID")
         }
+    }
+    
+    // 投稿の詳細をUserdefaultsに保存する
+    func savePostDetailtoUserdefautls(postedTime: String, updateTime: String, myUID: String, values: [String : Any?], selectedImgae: UIImage){
+        let defaults = UserDefaults.standard
+        var postDetail = PostDetail()
+        postDetail.PostedTime = postedTime
+        postDetail.UpdateTime = updateTime
+        postDetail.PostUser = myUID
+        postDetail.TeamName = values["TeamName"] as? String
+        postDetail.Category = values["Category"] as? String
+        postDetail.Prefecture = values["Prefecture"] as? String
+        postDetail.Place = values["Place"] as? String
+        postDetail.ApplyGender = values["ApplyGender"] as? String
+        postDetail.Timezone = Array(values["Timezone"] as! Set<String>)
+        postDetail.Image = UIImageJPEGRepresentation(selectedImgae, 0.5)!
+        postDetail.Position = values["Position"].unsafelyUnwrapped == nil ? Array() : Array(values["Position"] as! Set<String>)
+        postDetail.ApplyLevel = values["ApplyLevel"].unsafelyUnwrapped == nil ? Array() : Array(values["ApplyLevel"] as! Set<String>)
+        postDetail.GenderRatio = values["GenderRatio"].unsafelyUnwrapped == nil ? "" : values["GenderRatio"] as! String
+        postDetail.TeamLevel = values["TeamLevel"].unsafelyUnwrapped == nil ? "" : values["TeamLevel"] as! String
+        postDetail.NumMembers = values["NumMembers"].unsafelyUnwrapped == nil ? 0 : values["NumMembers"] as! Int
+        postDetail.Day = values["Day"].unsafelyUnwrapped == nil ? Array() : Array(values["Day"] as! Set<String>)
+        postDetail.MainAge = values["MainAge"].unsafelyUnwrapped == nil ? Array() : Array(values["MainAge"] as! Set<String>)
+        postDetail.Comments = values["Comments"].unsafelyUnwrapped == nil ? "" : values["Comments"] as! String
+        let data = try? JSONEncoder().encode(postDetail)
+        defaults.set(data ,forKey: "recruite")
+    }
+
+    
+    // ImageCellで選択された画像をFirebaseStorageに保存
+    func saveImagetoFirebaseStorage(directory: String, myUID: String, selectedImgae: UIImage) -> Bool{
+        //Firebase Storageの準備
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+        // UIImageJPEGRepresentationでUIImageをNSDataに変換して格納
+        var ImageShrinkRatio:CGFloat = 1.0
+        if var data = UIImageJPEGRepresentation(selectedImgae, ImageShrinkRatio){
+            //画像のファイルサイズが1024*1024 / 2bytes以下になるまで縮小係数を調整
+            while data.count > 1024 * 1024 / 2{
+                ImageShrinkRatio = ImageShrinkRatio - 0.1
+                data = UIImageJPEGRepresentation(selectedImgae, ImageShrinkRatio)!
+            }
+            //とりあえずUIDのディレクトリを作成し、その下に画像を保存
+            let reference = storageRef.child(myUID + "/" + directory + "/image.jpg")
+            reference.putData(data, metadata: nil, completion: { metaData, error in
+                print(metaData as Any)
+                print(error as Any)
+            })
+            return true
+        }
+        return false
     }
     
     
